e36d988f7a9cd04dce1ca001ff5c6bfb395746d5
studiominti <studiominti@gmail.com>
2020-02-11 17:28:03 -0300

    Initial commit and create books scaffold

---
+++ db/migrate/20200211202457_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.integer :status
+      t.string :lend_to
+
+      t.timestamps
+    end
+  end
+end
